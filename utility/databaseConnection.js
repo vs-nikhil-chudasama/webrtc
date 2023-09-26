0c41a5619397f517fdc21a0b1577884998295d53
Nikhil <nikhil@vsquare.tech>
2023-09-26 11:28:30 +0530

    fix db connection file

--- utility/databaseConnection.js
+++ utility/databaseConnection.js
@@ -3,7 +3,7 @@ var dbConnection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 module.exports = dbConnection;
